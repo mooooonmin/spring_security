529650f87e7d012d1968847ee6b7aaf52b4eee2c
mooooonmin <minhee610@gmail.com>
2025-08-27 10:21:08 +0900

    custom logout

--- src/main/java/io/security/springsecuritymaster/security/service/FormUserDetailsService.java
+++ src/main/java/io/security/springsecuritymaster/security/service/FormUserDetailsService.java
@@ -27,6 +27,7 @@ public class FormUserDetailsService implements UserDetailsService {
         Account account = userRepository.findByUsername(username);
         if (account == null) {
             throw new UsernameNotFoundException("No user found with username: " + username);
+
         }
         List<GrantedAuthority> authorities = List.of(new SimpleGrantedAuthority(account.getRoles()));
         ModelMapper mapper = new ModelMapper();
--- src/main/java/io/security/springsecuritymaster/users/repository/UserRepository.java
+++ src/main/java/io/security/springsecuritymaster/users/repository/UserRepository.java
@@ -4,6 +4,7 @@ import io.security.springsecuritymaster.domain.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Account, Long> {
+
     Account findByUsername(String username);
 
 }
